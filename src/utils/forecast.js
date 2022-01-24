f38abce3a5af6868d6460a039b9d80669bcd4baa
Polygon <polygon@Polygons-MBP.Home>
2022-01-24 13:58:50 +0000

    Foracst changes, timezone

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,14 +3,14 @@ const request = require('request')
 
 
 const geocode = (lat, long , callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=9b7ad69343f0622f15cc5a4597a693bf&query=' + long+','+ lat+ '&units=m'
+    const url = 'http://api.weatherstack.com/current?access_key=9b7ad69343f0622f15cc5a4597a693bf&query=' + lat+','+ long+ '&units=m'
     request({ url, json: true } , (error, {body}) => {
         if(error){
           callback('Unable to connect to location services' )
         }else if(body.error){
           callback('Unable to find location. Try another search')
         }else {
-          callback(undefined, body.current.weather_descriptions[0] +'. It is currently ' + body.current.temperature + ' degrees out, but it feels like ' + body.current.feelslike + ' degrees out there')
+          callback(undefined, body.current.weather_descriptions[0] +'. It is currently ' + body.current.temperature + ' last checked at ' + body.current.observation_time + ','+body.location.timezone_id)
         }
     })
 }
